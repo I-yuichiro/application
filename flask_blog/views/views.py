0ad235b8c5f1b446f0e0fe933538130904f51034
I-yuichiro <52876660+I-yuichiro@users.noreply.github.com>
2020-03-16 22:31:00 +0900

    app

--- flask_blog/views/views.py
+++ flask_blog/views/views.py
@@ -2,8 +2,21 @@
 
 from flask import request, redirect, url_for, render_template, flash, session
 from flask_blog import app
+from functools import wraps
 
-@app.route('/login', methods=["GET", 'POST'])
+
+
+def login_required(view):
+  @wraps(view)
+  def innner(*args, **kwargs):
+   if not session.get('logged_in'):
+    return redirect(url_for('login'))
+   return view(*args, **kwargs)
+  return innner
+
+
+
+@app.route('/login', methods=['GET', 'POST'])
 def login():
   error = None
   if request.method == 'POST':
@@ -17,8 +30,10 @@ def login():
       return redirect(url_for('show_entries'))
   return render_template('login.html')
 
+
+
 @app.route('/logout')
 def logout():
   session.pop('logged_in', None)
-  flash('ログアウトしました')
+  flash('ログインしました')
   return redirect(url_for('show_entries'))
